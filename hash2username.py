bf90939643d6eb1f09bf0c9457fb147e26f986aa
davidblakecoe <37048273+davidblakecoe@users.noreply.github.com>
2023-07-02 09:07:06 -0500

    Create hash2username.py
    I have dumped the NTDS.dit file and used hashcat to crack multiple hashes.  Then you have to tie that hash back to a username.  Hashcat does have a "--show --username" function (thanks hashcat developers).  Ive had it not work properly all the time.  This is a quick and dirty script to tie the username back to a cracked hash.

---
+++ hash2username.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import pandas as pandasForSortingCSV
+#open .csv files for processing
+df1 = pandasForSortingCSV.read_csv("ntds.csv")
+df2 = pandasForSortingCSV.read_csv("cracked.csv")
+#drop rows that have blank passwords
+df2 = df2.dropna()
+#default behavior only shows a sample of the output
+pd.set_option('display.max_rows', None)
+#merge fields based on hash
+data_merge = pd.merge(df1, df2)
+data_merge = data_merge.sort_index(ascending = False, axis = 1)
+print(data_merge)
